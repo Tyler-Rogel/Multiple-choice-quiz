3761ad7ceb5e5dc16022c3e946b8d22c138e4ae8
tyler rogel <trogel138@gmail.com>
2022-06-30 11:03:15 -0600

    Works but not really

--- app.js
+++ app.js
@@ -10,16 +10,16 @@ var app = new Vue({
         page: "titlePage",
         quiz: [
             {title: "Question 1",
-            questionText: "What is the 3rd letter in the alphabet?",
+            questionText: "What is your favorite color?",
             answers: [
-                {text: "a", correct: false},
-                {text: "b", correct: false},
-                {text: "c", correct: true},
-                {text: "d", correct: false}
+                {text: "red", correct: false},
+                {text: "blue", correct: false},
+                {text: "cyan", correct: true},
+                {text: "dark green", correct: false}
             ],
             userAnswer: ""},
             {title: "Question 2",
-            questionText: "What is the 3rd letter in the alphabet?",
+            questionText: "What is the 7th letter in the alphabet?",
             answers: [
                 {text: "a", correct: false},
                 {text: "b", correct: false},
@@ -28,12 +28,30 @@ var app = new Vue({
             ],
             userAnswer: ""},
             {title: "Question 3",
-            questionText: "What is the 3rd letter in the alphabet?",
+            questionText: "Which is the Animal?",
             answers: [
-                {text: "a", correct: false},
-                {text: "b", correct: false},
-                {text: "c", correct: true},
-                {text: "d", correct: false}
+                {text: "cat", correct: false},
+                {text: "rigby", correct: false},
+                {text: "dog", correct: true},
+                {text: "bird", correct: false}
+            ],
+            userAnswer: ""},
+            {title: "Question 4",
+            questionText: "????",
+            answers: [
+                {text: "?", correct: false},
+                {text: "?", correct: false},
+                {text: "?", correct: true},
+                {text: "?", correct: false}
+            ],
+            userAnswer: ""},
+            {title: "Question 5",
+            questionText: "hello?",
+            answers: [
+                {text: "hi", correct: false},
+                {text: "hello", correct: false},
+                {text: "no", correct: true},
+                {text: "wazzup", correct: false}
             ],
             userAnswer: ""}
         ]
@@ -57,7 +75,7 @@ var app = new Vue({
                     count += 1;
                 }
             }
-            if (count == 3){
+            if (count == 5){
                     console.log("Finished");
                     this.page = "scorePage";
                     console.log(count);
